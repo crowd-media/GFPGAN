8f1a5a1c8bed4fa7a38a96778ba2e14785ed2ac6
Marcel <marcel@aflorithmic.ai>
2022-08-02 13:29:05 +0000

    add init file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+
+
+import os
+import sys
+
+
+## add current dir to sys path so that root method py files can be imported from outside 
+current_dir = os.path.dirname( __file__ )
+sys.path.append( current_dir )
